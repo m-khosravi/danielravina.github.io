97b624ffd57ab6ab8056f92d45d0ef8d796827ec
DanielRavina <danielravina@gmail.com>
2015-10-14 20:01:31 -0700

    remove debug code

--- src/js/index.js
+++ src/js/index.js
@@ -3,13 +3,3 @@ var ReactDom = require('react-dom');
 var App = require('./components/App');
 
 ReactDom.render(<App />, document.getElementById('app'));
-
-
-
-var $ = require('jquery');
-
-$(document).ready(function(){
-  $("body").click(function(e) {
-    console.log(e)
-  })
-})
