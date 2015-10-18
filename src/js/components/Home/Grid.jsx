b14f62b7869cdf02d422fa40e9abf42be8fd48e2
DanielRavina <danielravina@gmail.com>
2015-10-17 22:30:53 -0700

    update builds

--- src/js/components/Home/Grid.jsx
+++ src/js/components/Home/Grid.jsx
@@ -37,7 +37,7 @@ var Grid = React.createClass({
           <Column key={i} columns={COLUMNS}>
             <div className={"lastColumn"}>
               <h3>Hi There!</h3>
-              <p>Found anything useful on this site? <a href={"#contact"}>Drop me a line</a>, I'd love to hear about it.</p>
+              <p>Found anything useful on this site? <a href="contact">Drop me a line</a>, I'd love to hear about it.</p>
             </div>
           </Column>
         );
