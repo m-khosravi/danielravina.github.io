3e48c1b55ec36ad371f49da090eed3f262113b73
DanielRavina <danielravina@gmail.com>
2015-10-19 08:31:56 -0700

    update about section

--- src/js/components/About/About.jsx
+++ src/js/components/About/About.jsx
@@ -1,6 +1,6 @@
 var React       = require('react');
 
-
+var YEAR_DEVELOP = 2011;
 var About = React.createClass({
   render: function() {
     return (
@@ -13,7 +13,7 @@ var About = React.createClass({
         <div className="row">
           <div className="medium-6 small-10 medium-centered small-centered columns">
             <p>
-              {"In my 4 years of industry experience, I was fortunate enough to work with some great clients and build cool stuff for them! Just to name a few - BC Liquor Distribution Branch, BC Hydro, UBC, Cisco, Live Nation, MHCC and more. I was involved in every aspect of the development cycle, starting from the discovery phase, requirements analysis, design, test, development, and deployment."}
+              {"Iv'e been developing software for"} {(new Date().getFullYear()) - YEAR_DEVELOP} years now. {"During that that time, I was fortunate enough to work with some great clients and build cool stuff for them! Just to name a few - BC Liquor Distribution Branch, BC Hydro, UBC, Cisco, Live Nation, MHCC and more. I was involved in every aspect of the development cycle, starting from the discovery phase, requirements analysis, design, test, development, and deployment."}
             </p>
             <p>
               {"My go-to languages at the moment are Javascript and Ruby. I love the stability, elegance and simplicity of Ruby and astonished by the possibilities and power of javascript with Node.js."}
@@ -22,7 +22,7 @@ var About = React.createClass({
               {"I earned 3 professional certificates from BCIT, all through part-time studies (Evenings and weekends) while working 40 - 60 hours a week. Now I'm on my way to complete a diploma in Computer Systems from BCIT."}
             </p>
             <p>
-              {"In my free time you'll probably find me at a local tech meetup, or experimenting with cool new technologies or contributing to open source projects."}
+              {"In my free time you'll probably find me at a"} <a target='_blank' href='http://www.meetup.com/members/145250932/'>local tech meetup</a>{", experimenting with cool new technologies or contributing to open source projects."}
             </p>
           </div>
         </div>
